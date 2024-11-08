13e5f71b3870d94496e6667fd34060e246fbd7ef
natavangurbanova <gurbanovanatavann@gmail.com>
2024-11-08 06:22:05 +0400

    navigation controller for tab bar controller

--- FirebaseInstaClone/TabBarViewController.swift
+++ FirebaseInstaClone/TabBarViewController.swift
@@ -16,10 +16,13 @@ class TabBarViewController: UITabBarController {
         let feedVC = FeedViewController()
         let profileVC = ProfileViewController()
         
+        let feedNavController = UINavigationController(rootViewController: feedVC)
+        let profileNavController = UINavigationController(rootViewController: profileVC)
+        
         feedVC.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "house.fill"), tag: 0)
         profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.fill"), tag: 1)
         
-        self.viewControllers = [feedVC, profileVC]
+        viewControllers = [feedNavController, profileNavController]
         
         
     }
