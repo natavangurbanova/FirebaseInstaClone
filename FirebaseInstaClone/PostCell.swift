e5d80024010f741e5fc64808adb9e313cb1f35f3
natavangurbanova <gurbanovanatavann@gmail.com>
2025-01-20 05:31:27 +0400

    Creating the PostCellVC

---
+++ FirebaseInstaClone/PostCell.swift
@@ -0,0 +1,41 @@
+//
+//  PostCell.swift
+//  FirebaseInstaClone
+//
+//  Created by Natavan Gurbanova on 17.01.25.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import SDWebImage
+
+class PostCell: UICollectionViewCell {
+    
+    private let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        contentView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func configure(with imageUrl: String) {
+        if let url = URL(string: imageUrl) {
+            imageView.sd_setImage(with: url, placeholderImage: UIImage(systemName: "photo"))
+        }
+    }
+    
+}
