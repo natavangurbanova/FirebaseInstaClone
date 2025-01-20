db3df03f63a0053ff4683c8a40ea0c6421212a71
natavangurbanova <gurbanovanatavann@gmail.com>
2025-01-20 05:41:01 +0400

    Changes

--- FirebaseInstaClone/ProfileViewController.swift
+++ FirebaseInstaClone/ProfileViewController.swift
@@ -11,7 +11,7 @@ import FirebaseAuth
 import FirebaseFirestore
 import SDWebImage
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     let profileImageView: UIImageView = {
         let imageView = UIImageView()
@@ -26,16 +26,15 @@ class ProfileViewController: UIViewController {
     let usernameLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.boldSystemFont(ofSize: 24)
-        label.text = "Username"
+        label.text = "username"
         return label
     }()
     
     let bioLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 14)
-        label.numberOfLines = 0
+        label.numberOfLines = 1
         label.textAlignment = .center
-        label.text = "This is your bio"
         return label
     }()
     
@@ -53,17 +52,88 @@ class ProfileViewController: UIViewController {
         button.setTitle("Log Out", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .black
-        button.layer.cornerRadius = 10
+        button.layer.cornerRadius = 8
         return button
     }()
+    
+    private let postsLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "0\nPosts"
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private let followersLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "0\nFollowers"
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private let followingLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "0\nFollowing"
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private lazy var statsStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [postsLabel, followersLabel, followingLabel])
+        stack.axis = .horizontal
+        statsStackView.layer.borderColor = UIColor.lightGray.cgColor
+        statsStackView.layer.borderWidth = 1.0
+        statsStackView.layer.cornerRadius = 5
+        stack.distribution = .fillEqually
+        stack.spacing = 8
+        return stack
+    }()
+    
+    private var posts: [Post] = []
+    private var collectionView: UICollectionView!
+    
+    private let placeholderView = UIView()
+        private let cameraImageView: UIImageView = {
+            let imageView = UIImageView(image: UIImage(systemName: "camera"))
+            imageView.tintColor = .gray
+            imageView.contentMode = .scaleAspectFit
+            return imageView
+        }()
+        private let sharePhotosLabel: UILabel = {
+            let label = UILabel()
+            label.text = "Share photos"
+            label.font = UIFont.boldSystemFont(ofSize: 24)
+            label.textColor = .darkGray
+            return label
+        }()
+        private let sharePhotosDescriptionLabel: UILabel = {
+            let label = UILabel()
+            label.text = "When you share photos, they will appear on your profile"
+            label.font = UIFont.systemFont(ofSize: 14)
+            label.textColor = .gray
+            label.textAlignment = .center
+            label.numberOfLines = 0
+            return label
+        }()
+
     
     //MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(reloadProfileData), name: .didUpdateProfileImage, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadProfileData), name: .didUpdateProfileData, object: nil)
 
         view.backgroundColor = .white
         setupViews()
+        setupStackView()
+        setupPlaceholderView()
+        setupCollectionView()
+        loadPosts()
     }
     @objc func reloadProfileData() {
         loadProfileData()
@@ -72,7 +142,50 @@ class ProfileViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         loadProfileData()
+        let currentUserId = Auth.auth().currentUser?.uid ?? ""
+            updatePostCount(for: currentUserId)
+            updateFollowerCount(for: currentUserId)
+            updateFollowingCount(for: currentUserId)
     }
+    
+    func updatePostCount(for userID: String) {
+        let postsRef = Firestore.firestore().collection("posts").whereField("ownerId", isEqualTo: userID)
+        postsRef.getDocuments { [weak self] snapshot, error in
+            guard let self = self else { return }
+            if let count = snapshot?.documents.count {
+                self.postsLabel.text = "\(count)\nPosts"
+            } else {
+                if let error = error {
+                    print("Error fetching data: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    func updateFollowerCount(for userID: String) {
+        let followersRef = Firestore.firestore().collection("users").document(userID).collection("followers")
+        followersRef.getDocuments { snapshot, error in
+            if let count = snapshot?.documents.count {
+                self.followersLabel.text = "\(count)\nFollowers"
+            } else {
+                if let error = error {
+                    print("Error fetching data: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    func updateFollowingCount(for userID: String) {
+        let followingRef = Firestore.firestore().collection("users").document(userID).collection("following")
+        followingRef.getDocuments { snapshot, error in
+            if let count = snapshot?.documents.count {
+                self.followingLabel.text = "\(count)\nFollowing"
+            } else {
+                if let error = error {
+                    print("Error fetching data: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+
     
     func loadProfileData() {
         guard let userID = Auth.auth().currentUser?.uid else { return }
@@ -80,17 +193,21 @@ class ProfileViewController: UIViewController {
         
         db.collection("users").document(userID).getDocument { [weak self] document, error in
             guard let self = self else { return }
+            
             if let document = document, document.exists {
                 let data = document.data()
                 
-                // Fetch username and bio
+                // Use username from Firestore or fallback to defaultUsername
                 self.usernameLabel.text = data?["username"] as? String ?? "No username"
-                self.bioLabel.text = data?["bio"] as? String ?? "No bio"
+
+                
+                // Fetch bio
+                self.bioLabel.text = data?["bio"] as? String ?? "Add your bio"
                 
                 // Fetch and load profile image
-                if let profileImageUrl = data?["profileImageURL"] as? String, let url = URL(string: profileImageUrl) {
+                if let profileImageUrl = data?["profileImageUrl"] as? String, let url = URL(string: profileImageUrl) {
                     print("Profile Image URL: \(profileImageUrl)") // Debug log
-                    self.setImage(with: url)
+                    self.profileImageView.sd_setImage(with: url, placeholderImage: UIImage(systemName: "person.circle"))
                 } else {
                     print("No profileImageUrl found")
                     self.profileImageView.image = UIImage(systemName: "person.circle")
@@ -100,13 +217,8 @@ class ProfileViewController: UIViewController {
             }
         }
     }
-    
-    fileprivate func setImage(with URL: URL) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.profileImageView.sd_setImage(with: URL)
-        }
-    }
-    
+
+   
     private func setupViews() {
         view.addSubview(profileImageView)
         view.addSubview(usernameLabel)
@@ -117,32 +229,134 @@ class ProfileViewController: UIViewController {
         logOutButton.addTarget(self, action: #selector(logOut), for: .touchUpInside)
         
         profileImageView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(40)
-            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
             make.width.height.equalTo(100)
         }
         usernameLabel.snp.makeConstraints { make in
-            make.top.equalTo(profileImageView.snp.bottom).offset(20)
-            make.centerX.equalToSuperview()
+            make.top.equalTo(profileImageView.snp.top)
+            make.leading.equalTo(profileImageView.snp.trailing).offset(30)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
         }
         bioLabel.snp.makeConstraints { make in
-            make.top.equalTo(usernameLabel.snp.bottom).offset(10)
-            make.horizontalEdges.equalToSuperview().inset(20)
+            make.top.equalTo(profileImageView.snp.bottom).offset(16)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+            make.trailing.lessThanOrEqualTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
         }
         editProfileButton.snp.makeConstraints { make in
-            make.top.equalTo(bioLabel.snp.bottom).offset(20)
-            make.centerX.equalToSuperview()
-            make.width.equalTo(150)
-            make.height.equalTo(50)
+            make.top.equalTo(usernameLabel.snp.bottom).offset(20)
+            make.leading.equalTo(profileImageView.snp.trailing).offset(30)
+            //make.width.equalTo(120)
+            make.height.equalTo(40)
+            //make.trailing.equalTo(logOutButton.snp.leading).offset(-10)
         }
         logOutButton.snp.makeConstraints { make in
-            make.top.equalTo(editProfileButton.snp.bottom).offset(20)
-            make.center.equalToSuperview()
-            make.width.equalTo(150)
+            make.top.equalTo(editProfileButton)
+            //make.top.equalTo(usernameLabel.snp.bottom).offset(20)
+            make.leading.equalTo(editProfileButton.snp.trailing).offset(10)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-20)
+            make.width.equalTo(editProfileButton)
+            //make.width.equalTo(80)
+            //make.height.equalTo(40)
+        }
+    }
+    
+    private func setupPlaceholderView() {
+            view.addSubview(placeholderView)
+            placeholderView.snp.makeConstraints { make in
+                make.top.equalTo(statsStackView.snp.bottom).offset(20)
+                make.left.right.equalToSuperview().inset(20)
+                make.bottom.equalToSuperview().inset(50) // Adjust as needed
+            }
+            
+            placeholderView.addSubview(cameraImageView)
+            cameraImageView.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.top.equalToSuperview().offset(30)
+                make.width.height.equalTo(50)
+            }
+
+            placeholderView.addSubview(sharePhotosLabel)
+            sharePhotosLabel.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.top.equalTo(cameraImageView.snp.bottom).offset(16)
+            }
+
+            placeholderView.addSubview(sharePhotosDescriptionLabel)
+            sharePhotosDescriptionLabel.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.top.equalTo(sharePhotosLabel.snp.bottom).offset(8)
+                make.left.right.equalToSuperview().inset(20)
+            }
+        }
+    
+    private func setupStackView() {
+        view.addSubview(statsStackView)
+        statsStackView.snp.makeConstraints { make in
+            make.top.equalTo(bioLabel.snp.bottom).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+            make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
             make.height.equalTo(50)
         }
-        
     }
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cellSize = (view.frame.width - 2) / 3
+        layout.itemSize = CGSize(width: cellSize, height: cellSize)
+        layout.minimumInteritemSpacing = 1
+        layout.minimumLineSpacing = 1
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(PostCell.self, forCellWithReuseIdentifier: "PostCell")
+        
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(statsStackView.snp.bottom).offset(20)
+            make.left.right.bottom.equalToSuperview()
+        }
+    }
+    
+    private func loadPosts() {
+        guard let userID = Auth.auth().currentUser?.uid else { return }
+        let db = Firestore.firestore()
+        
+        db.collection("posts").document(userID).collection("userPosts").order(by: "timestamp", descending: true).getDocuments { snapshot, error in
+               if let error = error {
+                   print("Error fetching posts: \(error.localizedDescription)")
+                   return
+               }
+                    self.posts = snapshot?.documents.compactMap { doc -> Post in
+                        let data = doc.data()
+                        //let imageUrl = data["imageUrl"] as? String ?? ""
+                        return Post(id: doc.documentID, imageUrl: data["imageUrl"] as? String ?? "", caption: data["caption"] as? String ?? "")
+                    } ?? []
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return posts.count
+       }
+
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostCell", for: indexPath) as! PostCell
+           let post = posts[indexPath.row]
+           cell.configure(with: post.imageUrl)
+           return cell
+       }
+
+       func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+           let post = posts[indexPath.row]
+           let fullImageVC = FullImageViewController()
+           fullImageVC.postImageURL = post.imageUrl
+           navigationController?.pushViewController(fullImageVC, animated: true)
+       }
     
     @objc func editProfileTapped() {
         let editProfileVC = EditProfileViewController()
@@ -166,5 +380,6 @@ class ProfileViewController: UIViewController {
 
 extension Notification.Name {
     static let didUpdateProfileImage = Notification.Name("didUpdateProfileImage")
+    static let didUpdateProfileData = Notification.Name("didUpdateProfileData")
 }
 
