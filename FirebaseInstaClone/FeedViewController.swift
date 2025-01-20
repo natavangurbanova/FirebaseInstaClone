2747e1b2ec89da108678b69d30048db4d21ecb6b
natavangurbanova <gurbanovanatavann@gmail.com>
2025-01-20 05:30:54 +0400

    changes fo the FeedVC

--- FirebaseInstaClone/FeedViewController.swift
+++ FirebaseInstaClone/FeedViewController.swift
@@ -10,29 +10,70 @@ import SnapKit
 
 class FeedViewController: UIViewController {
     
+    private let feedCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.layer.borderWidth = 2.0
+        collectionView.layer.borderColor = UIColor.white.cgColor
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         title = "Feed"
+        setupCollectionView()
         
-        setupLayout()
     }
     
-    private func setupLayout() {
-        let label = UILabel()
-        label.text = "Welcome to the Feed"
-        label.textColor = .black
-        view.addSubview(label)
-        
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
-    }
-    
-    
-    
-    
-    
-        
 }
+
+extension FeedViewController {
+    private func setupCollectionView() {
+        view.addSubview(feedCollectionView)
+        
+        feedCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.horizontalEdges.equalToSuperview()
+        }
+        
+        feedCollectionView.delegate = self
+        feedCollectionView.dataSource = self
+        feedCollectionView.register(FeedCell.self, forCellWithReuseIdentifier: "FeedCell")
+    }
+}
+
+extension FeedViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedCell", for: indexPath) as! FeedCell
+        cell.postImageView.image = UIImage(named: "sampleImage")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 300)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+}
+    
+    
+    
+    
+    
+    
+    
+        
+
