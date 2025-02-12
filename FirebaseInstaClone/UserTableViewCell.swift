fbe36eb1828e75040f0065a7472672b2c03b10d7
natavangurbanova <gurbanovanatavann@gmail.com>
2025-02-13 00:33:16 +0400

    creating new cell class for search VC

---
+++ FirebaseInstaClone/UserTableViewCell.swift
@@ -0,0 +1,81 @@
+//
+//  UserTableViewCell.swift
+//  FirebaseInstaClone
+//
+//  Created by Natavan Gurbanova on 10.02.25.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+
+class UserTableViewCell: UITableViewCell {
+    
+    static let reuseIdentifier = "UserCell"
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 20
+        return imageView
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(usernameLabel)
+        
+        profileImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(40)
+        }
+        
+        usernameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(profileImageView.snp.trailing).offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.centerY.equalToSuperview()
+        }
+        usernameLabel.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private var imageUrl: URL?
+    
+    func configure(with user: User) {
+        usernameLabel.text = user.username
+        
+        guard let profileImageUrl = user.profileImageUrl, let url = URL(string: profileImageUrl) else {
+            profileImageView.image = UIImage(systemName: "person.circle")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+               if let error = error {
+                   print("Failed to load image: \(error.localizedDescription)")
+               }
+               
+               if let data = data, let image = UIImage(data: data) {
+                   DispatchQueue.main.async {
+                       self.profileImageView.image = image
+                   }
+               } else {
+                   DispatchQueue.main.async {
+                       self.profileImageView.image = UIImage(systemName: "person.circle")
+                   }
+               }
+           }.resume()
+       }
+}
