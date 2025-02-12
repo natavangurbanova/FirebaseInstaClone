ca38bfaf9aca9daf0b6c6a9acd4025b8f72bed5f
natavangurbanova <gurbanovanatavann@gmail.com>
2025-02-12 22:39:56 +0400

    modify the order of view controllers

--- FirebaseInstaClone/TabBarViewController.swift
+++ FirebaseInstaClone/TabBarViewController.swift
@@ -16,16 +16,18 @@ class TabBarViewController: UITabBarController {
         let feedVC = FeedViewController()
         let profileVC = ProfileViewController()
         let uploadVC = UploadViewController()
+        let searchVC = SearchViewController()
         
         let feedNavController = UINavigationController(rootViewController: feedVC)
         let profileNavController = UINavigationController(rootViewController: profileVC)
         let uploadNavController = UINavigationController(rootViewController: uploadVC)
+        let searchNavController = UINavigationController(rootViewController: searchVC)
         
         feedVC.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "house.fill"), tag: 0)
-        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.fill"), tag: 1)
+        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.fill"), tag: 3)
         uploadVC.tabBarItem = UITabBarItem(title: "Upload", image: UIImage(systemName: "plus.app"), tag: 2)
-        viewControllers = [feedNavController, profileNavController, uploadNavController]
-        
+        searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass.circle.fill"), tag: 1)
+        viewControllers = [feedNavController,searchNavController,uploadNavController,profileNavController]
         
     }
     
